36fc7cc4441bcaee2f51bebccb8fe7fca8466bbc
ToastL <etiendenouden2006@gmail.com>
2024-12-19 09:02:33 +0100

    idk

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,7 @@ import { EngineImage } from "./engine/image";
 import { EngineObject } from "./engine/object";
 
 class Client extends Engine {
+  private playerImages: EngineImage[]
   private player: EngineObject
   
   private playerX: number = 0
@@ -12,24 +13,50 @@ class Client extends Engine {
   private aDown = false
   private dDown = false
 
+  private playerFrame = 0
+
   constructor(ctx: CanvasRenderingContext2D) {
     super(ctx, 800, 500)
 
     this.player = new EngineObject(32, 32, 0, 0)
+    this.playerImages = []
   }
 
   public async init() {
-    const playerImage = new EngineImage()
-    await playerImage.loadPNG("/images/standing_gasmask/frame1.png")
+    for (let i = 0; i < 4; i++) {
+      const playerImage = new EngineImage()
 
-    this.player.setImage(playerImage)
+      await playerImage.loadPNG(`/images/standing_gasmask/frame${i+1}.png`)
+
+      this.playerImages.push(playerImage) 
+    }
+
+    this.player.setImage(this.playerImages[this.playerFrame])
   }
 
   public update(dt: number) {
-    if (this.aDown)
-        this.playerX -= 40*dt
-    if (this.dDown)
-        this.playerX += 40*dt
+    let moved = false
+    if (this.aDown) {
+      this.playerX -= 40*dt
+      
+      moved = true
+    }
+    if (this.dDown) {
+      this.playerX += 40*dt
+      
+      if (moved) moved = false
+      else moved = true
+    }
+
+    if (!moved) {
+      this.playerFrame += 5 * dt
+      if (this.playerFrame > 4) this.playerFrame = 0
+      this.player.setImage(this.playerImages[Math.floor(this.playerFrame)])
+    } else {
+      this.playerFrame = 0
+      this.player.setImage(this.playerImages[Math.floor(this.playerFrame)])
+    }
+
     this.player.setPosX(this.playerX)
     
     this.player.setPosY(this.playerY)
